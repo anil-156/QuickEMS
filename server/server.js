7e4968170ffecc6a206cfff52ff8f49317e7127d
anil-156 <sawantanil0612@gmail.com>
2026-09-01 22:07:21 +0530

    profile controller completed

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ import express from "express";
 import cors from "cors";
 import multer from "multer";
 import connectDB from "./config/db.js";
+import authRouter from "./routes/authRoutes.js";
+import employeesRouter from "./routes/employeeRoutes.js";
+import profileRouter from "./routes/profileRoutes.js";
 
 const app = express()
 const PORT = process.env.PORT || 4000;
@@ -13,7 +16,10 @@ app.use(express.json())
 app.use(multer().none())
 
 //routes
-app.get("/", (req,res)=>res.send("Server is running"))
+app.get("/", (req,res)=>res.send("Server is running"));
+app.use("/api/auth", authRouter)
+app.use("/api/employees", employeesRouter)
+app.use("/api/profile", profileRouter)
 
 await connectDB()
 app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`))
